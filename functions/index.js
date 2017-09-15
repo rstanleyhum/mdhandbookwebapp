1b9191f06952aa40f3587e1334d96d7e510f649c
R. Stanley Hum <r.stanley.hum@gmail.com>
2017-09-15 10:09:44 -0400

    removed indexfile creation; simplified to bookpages and stylefiles functions

--- functions/index.js
+++ functions/index.js
@@ -9,118 +9,51 @@ const indexFilename = "indexfile.json";
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-// exports.updateindex = functions.storage.object().onChange(event => {
-//   const object = event.data;
-//   const filePath = object.name;
-
-//   if (filePath == indexFilename) {
-//     return;
-//   }
-  
-//   const bucket = gcs.bucket(storageBucketName);
-//   var indexfile = bucket.file(indexFilename);
-  
-//   _updateIndex(bucket, indexfile)
-//     .then( () => {
-//       return;
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-
-//   return;
-// });
-
-
-const _updateIndex = (bucket, indexfile) => {
-  var p = new Promise( (resolve, reject) => {
-    _getFilesFromBucket(bucket)
-      .then(data => {
-        var files = data[0];
-        return _getMetadata(files);
-      })
-      .then(metadata => {
-        return _parseMetadata(metadata);
-      })
-      .then(json => {
-        return _saveToIndexFile(indexfile, json);
-      })
-      .then( () => {
-        resolve();
-      })
-      .catch( err => {
-        reject(err);
-      });
-  });
-  return p;
-}
-
-const _getFilesFromBucket = (bucket) => {
-  return bucket.getFiles();
-};
-
-const _getMetadata = (files) => {
-  var getallmetadata = files.map(item => {
-    return item.getMetadata()
-  });
-  return Promise.all(getallmetadata);
-};
-
-const _parseMetadata = (metadata) => {
-  return metadata.map(meta => {
-    var item = {};
-    item.name = meta[0].name;
-    item.updated = Date.parse(meta[0].updated);
-    return item;
-  });
-};
-
-const _saveToIndexFile = (indexfile, json) => {
-  return indexfile.save(JSON.stringify(json, null, 2));
-}
-
-exports.httpsupdateindex = functions.https.onRequest((request, response) => {
+exports.bookpages = functions.https.onRequest((request, response) => {
   const bucket = gcs.bucket(storageBucketName);
-  var indexfile = bucket.file(indexFilename);
-  _updateIndex(bucket, indexfile)
-    .then( () => {
-      response.end();
-    })
-    .catch(err => {
-      response.status(500).json({"error": err});
-    })
-});
-
-
-exports.checkfiles = functions.https.onRequest((request, response) => {
-  const bucket = gcs.bucket(storageBucketName);
-  var indexfile = bucket.file(indexFilename);
-  
+  var query = {
+    prefix: 'bookpages/',
+    delimiter: '/',
+  };
   var checkDate = _getCheckDate(request.query.time);
 
-  indexfile.get()
+  bucket.getFiles(query)
     .then(data => {
-      var file = data[0];
-      var fileUpdated = new Date(file.metadata.updated)
-      
-      if (fileUpdated < checkDate) {
-        var returnData = [];
-        var content = [];
-        returnData[0] = JSON.stringify(content);
-        return Promise.resolve(returnData);
-      }
-      return file.download();
-    })
-    .then(data => {
-      var contents = data[0];
-      var filedata = JSON.parse(contents);
-      var json = _getJSON(filedata, checkDate.getTime());
+      var files = data[0];
+      var names = files.map(f => _returnFilenameIfUpdated(f.metadata.name, new Date(f.metadata.updated), checkDate));
+      var json = names.filter(n => n != null);
       response.json(json);
     })
     .catch( err => {
       response.status(500).json({"error": err});
     });
 });
+
+
+exports.stylefiles = functions.https.onRequest((request, response) => {
+  const bucket = gcs.bucket(storageBucketName);
+  var query = {
+    prefix: 'stylefiles/',
+    delimiter: '/',
+  };
+  var checkDate = _getCheckDate(request.query.time);
+
+  bucket.getFiles(query)
+    .then(data => {
+      var files = data[0];
+      var names = files.map(f => _returnFilenameIfUpdated(f.metadata.name, new Date(f.metadata.updated), checkDate));
+      var json = names.filter(n => n != null);
+      response.json(json);
+    })
+    .catch( err => {
+      response.status(500).json({"error": err});
+    });
+});
+
+
+const _returnFilenameIfUpdated = (name, lastupdated, checkdate) => {
+  return lastupdated > checkdate ? name : null;
+}
 
 
 const _getCheckDate = (timestring) => {
@@ -130,37 +63,3 @@ const _getCheckDate = (timestring) => {
   }
   return new Date(checkTime);
 }
-
-
-const _getJSON = (filedata, check) => {
-  var updateFiles = filedata.map(item => {
-    if (check < item.updated) {
-      return item.name;
-    } else {
-      return null;
-    }
-  });
-  var filteredFiles = updateFiles.filter(item => item != null);
-  var finalFiles = filteredFiles.filter(item => item != indexFilename);
-  return finalFiles;
-};
-
-
-exports.forcecheckfiles = functions.https.onRequest((request, response) => {
-  const bucket = gcs.bucket(storageBucketName);
-  var indexfile = bucket.file(indexFilename);
-  
-  var checkDate = _getCheckDate(request.query.time)
-  
-  indexfile.download()
-    .then(data => {
-      var contents = data[0]
-      var filedata = JSON.parse(contents);
-      var json = _getJSON(filedata, checkDate.getTime());
-      response.json(json);
-    })
-    .catch(err => {
-      response.status(500).json({"error": err});
-    })
-});
-
